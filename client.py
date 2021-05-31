2d11ab8776026b7075d9f49cf81d6e8cada3bf54
zaxon02 <zaxon02@gmail.com>
2021-05-31 20:38:39 +0300

    V1

--- client.py
+++ client.py
@@ -6,7 +6,7 @@ HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "192.168.1.26"
+SERVER = "192.168.0.103"
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,7 +16,7 @@ while True:
     full_msg = b''
     new_msg = True
     while True:
-        msg = s.recv(16)
+        msg = client.recv(16)
         if new_msg:
             print(f'new message length: {msg[:HEADERSIZE]}')
             msglen = int(msg[:HEADERSIZE])
@@ -28,8 +28,8 @@ while True:
             print('full msg recvd')
             print(full_msg[HEADERSIZE:])
 
-            d = pickle.loads(full_msg[HEADERSIZE:])
-            print(d)
+            bfolder = pickle.loads(full_msg[HEADERSIZE:])
+            print(bfolder)
 
 
             new_msg = True
