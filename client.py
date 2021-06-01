049f952b1e9c1b1e5e5c502a0c8c70f398d8fc8f
zaxon02 <zaxon02@gmail.com>
2021-06-01 21:37:45 +0300

    new

--- client.py
+++ client.py
@@ -1,40 +1,41 @@
 import socket
 import pickle
 
-HEADERSIZE = 10
-HEADER = 64
-PORT = 5050
+
+PORT = 9090
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "192.168.0.103"
-ADDR = (SERVER, PORT)
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(('', PORT))
 
-while True:
-    full_msg = b''
-    new_msg = True
     while True:
-        msg = client.recv(16)
-        if new_msg:
-            print(f'new message length: {msg[:HEADERSIZE]}')
-            msglen = int(msg[:HEADERSIZE])
-            new_msg = False
-        full_msg += msg
+        full_msg = b''
+        new_msg = True
+        while True:
+            msg = s.recv(1024)
+            if new_msg:
+                print(f'new message length: {msg[:HEADERSIZE]}')
+                msglen = int(msg[:HEADERSIZE])
+                new_msg = False
+            full_msg += msg
 
-        if len(full_msg) - HEADERSIZE == msglen:
-            print('full msg recvd')
-            print(full_msg[HEADERSIZE:])
+            if len(full_msg) - HEADERSIZE == msglen:
+                print('full msg recvd')
+                print(full_msg[HEADERSIZE:])
 
-            bfolder = pickle.loads(full_msg[HEADERSIZE:])
-            print(bfolder)
+                bfolder = pickle.loads(full_msg[HEADERSIZE:])
+                print(bfolder)
 
-            new_msg = True
-            full_msg = b''
+                new_msg = True
+                full_msg = b''
 
-            file = open('res.txt', 'w')
-            file.write(full_msg)
-            file.close()
+                file = open('res.txt', 'w')
+                file.write(full_msg)
+                file.close()
 
-    print(full_msg)
+        print(full_msg)
+
+if __name__ == "__main__":
+    main()
--- test.py
+++ test.py
@@ -3,13 +3,11 @@ import pickle
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import datetime
 
-ip = ['222.222.0.222', '111.111.0.111']
+CLIENTS = {'user1': '111.000.0.000', 'user2': '111.111.0.111'}
 
 HEADERSIZE = 10
-SERVER = socket.gethostbyname(socket.gethostname())
-PORT = 5050
-ADDR = (SERVER, PORT)
-print(SERVER)
+PORT = 9090
+#ADDR = (SERVER, PORT)
 
 
 class requestHandler(BaseHTTPRequestHandler):
@@ -19,44 +17,35 @@ class requestHandler(BaseHTTPRequestHandler):
         self.end_headers()
         device_url = self.path.split('/')
         print(str(device_url[-1]))
-        for i in ip:
-            if i == device_url[-1]:
-                print("your i is", i)
 
-        for i in ip:
-            if self.path.endswith('/time/' + i):
-                output = "OK" + ' ' + str(datetime.datetime.now().time())
-                self.wfile.write(output.encode())
-                response = open('res.txt', 'w')
-                response.write(output)
-                response.close()
-
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.bind(ADDR)
-                print("HOSTNAME IS --------> ", socket.gethostname())
-                s.listen(5)
-
-                while True:
-                    clientsocket, adress = s.accept()
-                    print(f"Connection from {adress} has been established!")
-                    print(clientsocket, " ", adress)
-
-                    d = output
-                    msg = pickle.dumps(d)
-
-                    msg = bytes(f"{len(msg):<{HEADERSIZE}}", "utf-8") + msg
-
-                    clientsocket.send(msg)
 
         if self.path.endswith('/time'):
-            output = ''
-            output += '<html><body>'
-            output = str(datetime.datetime.now().time())
-            output += '</body></html>'
+            output = "OK" + ' ' + str(datetime.datetime.now().time())
             self.wfile.write(output.encode())
-            response = open('request.txt', 'w')
+            response = open('res.txt', 'w')
             response.write(output)
             response.close()
+
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            print("Server is running")
+
+            d = output
+            msg = pickle.dumps(d)
+
+            msg = bytes(f"{len(msg):<{HEADERSIZE}}", "utf-8") + msg
+
+            x = s.sendto(msg, (CLIENTS['user1'], PORT))
+            s.close()
+
+        #if self.path.endswith('/time'):
+           # output = ''
+           # output += '<html><body>'
+           # output = str(datetime.datetime.now().time())
+           # output += '</body></html>'
+           # self.wfile.write(output.encode())
+           # response = open('request.txt', 'w')
+           # response.write(output)
+            #response.close()
 
         if self.path.endswith('/date'):
             output = ''
